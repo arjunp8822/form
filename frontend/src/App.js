972d77f849b3325932f9d3f4c4c4c59393e0d8c5
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2023-09-12 20:03:00 +1000

    changed elements to select

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,34 @@ import { react, useState } from "react";
 
 function App() {
   const [question, setQuestion] = useState(1);
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+  const [turnover, setTurnover] = useState("");
+  const [website, setWebsite] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const data = {
+      name,
+      type,
+      turnover,
+      website,
+    };
+    console.log(data);
+  };
+
   return (
     <div className="App">
       <div className="container">
         <div className="boxes">
           <form className={question === 1 ? "box show" : "box end"}>
             <div className="input-container">
-              <input type="text" placeholder="Enter your business name" />
+              <input
+                type="text"
+                placeholder="Enter your business name"
+                required
+                onChange={(e) => setName(e.target.value)}
+              />
               <button onClick={() => setQuestion(2)} type="button">
                 Next
               </button>
@@ -25,7 +46,21 @@ function App() {
             }
           >
             <div className="input-container">
-              <input type="text" placeholder="Enter your business type" />
+              <select
+                name="business-type"
+                required
+                id="business-type"
+                onChange={(e) => setType(e.target.value)}
+              >
+                <option value="" disabled selected>
+                  Select your business type
+                </option>
+                <option value="sole-trader">Sole Trader</option>
+                <option value="partnership">Partnership</option>
+                <option value="company">Company</option>
+                <option value="trust">Trust</option>
+                <option value="other">Other</option>
+              </select>
               <button onClick={() => setQuestion(3)} type="button">
                 Next
               </button>
@@ -43,7 +78,25 @@ function App() {
             }
           >
             <div className="input-container">
-              <input type="text" placeholder="Enter your business turnover" />
+              <select
+                name="business-turnover"
+                required
+                id="business-turnover"
+                onChange={(e) => setTurnover(e.target.value)}
+              >
+                <option value="" disabled selected>
+                  Select your turnover
+                </option>
+                <option value="Under $100,000">Under $100,000</option>
+                <option value="$100,000 - $500,000">$100,000 - $500,000</option>
+                <option value="$500,000 - $1 million">
+                  $500,000 - $1 million
+                </option>
+                <option value="$1 million - $5 million">
+                  $1 million - $5 million
+                </option>
+                <option value="Over $5 million">Over $5 million</option>
+              </select>
               <button onClick={() => setQuestion(4)} type="button">
                 Next
               </button>
@@ -60,8 +113,14 @@ function App() {
                 : "box show"
             }
           >
-            <div className="input-container">
-              <input type="text" placeholder="Enter your business website" />
+            <div className="input-container" required>
+              <input
+                type="text"
+                placeholder="Enter your business website"
+                required
+                onChange={(e) => setWebsite(e.target.value)}
+              />
+              <button onClick={submitHandler}>Next</button>
             </div>
           </form>
         </div>
